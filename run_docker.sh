3fc24888d007603d0facadb8ddeb8b74e23fb543
Bernd Kiefer <j_a_w_bone@gmx.de>
2025-02-10 16:46:49 +0100

    dockerize

---
+++ run_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#set -xe
+if test -z "$1"; then
+    echo "Usage: $0 <config.yml>"
+    exit 1
+fi
+scrdir=`dirname "$0"`
+docker run -it \
+       -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+       --add-host host.docker.internal=host-gateway \
+       -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+       -v $HOME/.config/pulse/cookie:/root/.config/pulse/cookie \
+       -v "$scrdir/$1":/app/config.yml \
+       -v "$scrdir/whisper-models":/app/whisper-models \
+       -v "$scrdir/audio":/app/audio \
+       -v "$scrdir/outputs":/app/outputs \
+       --gpus=all \
+       --entrypoint /bin/bash \
+       whisper_asr -c "./run_whisper.sh -c config.yml"
