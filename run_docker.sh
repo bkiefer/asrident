a3fe2e05aa03383076f2f3c370cc65187c52054e
Bernd Kiefer <bernd.kiefer@dfki.de>
2025-02-11 11:43:57 +0100

    fix run_docker

--- run_docker.sh
+++ run_docker.sh
@@ -11,6 +11,7 @@ docker run -it \
        -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
        -v $HOME/.config/pulse/cookie:/root/.config/pulse/cookie \
        -v "$scrdir/$1":/app/config.yml \
+       -v "$scrdir/pretrained_models":/app/pretrained_models \
        -v "$scrdir/whisper-models":/app/whisper-models \
        -v "$scrdir/audio":/app/audio \
        -v "$scrdir/outputs":/app/outputs \
