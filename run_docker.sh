2d7f4133535ee72a62e246864abdeed7bf6146d5
Bernd Kiefer <bernd.kiefer@dfki.de>
2025-02-11 15:32:48 +0100

    better options

--- run_docker.sh
+++ run_docker.sh
@@ -7,7 +7,7 @@ fi
 scrdir=`dirname "$0"`
 docker run -it \
        -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
-       --add-host host.docker.internal=host-gateway \
+       --add-host host.docker.internal:host-gateway \
        -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
        -v $HOME/.config/pulse/cookie:/root/.config/pulse/cookie \
        -v "$scrdir/$1":/app/config.yml \
