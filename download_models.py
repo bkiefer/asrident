56bd8f11360ecc37a00f10fce4483d5a92925c41
Bernd Kiefer <j_a_w_bone@gmx.de>
2026-06-20 15:54:31 +0200

    Fix model download scripts

--- download_models.py
+++ download_models.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S uv run
+#!/usr/bin/env -S uv run --no-sync
 from speechbrain.inference import EncoderClassifier, LocalStrategy
 from faster_whisper import utils
 import sys
@@ -7,7 +7,7 @@ for model in sys.argv[1:] if len(sys.argv) > 1 else ['large-v3-turbo']:
     utils.download_model(model, cache_dir='models/whisper/',
                          output_dir='models/whisper/' + model)
 
-EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb",
-                               savedir='models/spkrec-ecapa-voxceleb',
-                               local_strategy=LocalStrategy.COPY_SKIP_CACHE,
-                               huggingface_cache_dir="models/spkrec-ecapa-voxceleb")
+EncoderClassifier.from_hparams(
+    source="speechbrain/spkrec-ecapa-voxceleb",
+    savedir='models/spkrec-ecapa-voxceleb',
+    local_strategy=LocalStrategy.COPY_SKIP_CACHE)
