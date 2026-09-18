4cb8ddd07c2eb2619424283268651029417384d2
Bernd Kiefer <j_a_w_bone@gmx.de>
2026-09-18 13:55:00 +0200

    Improve docker build

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker build -f whisper-gstreamer/Dockerfile_mypy3_11 -t mypy:3.11 .
+cd whisper-gstreamer
+. utils.sh
+docker build -f Dockerfile_mypy3_11 -t mypy:3.11 .
+cd ..
 
 version=`grep version pyproject.toml | sed 's/version *= *"\([^"]*\)".*/\1/'`
-docker build -f Dockerfile -t asrident:"$version" .
+docker build -f Dockerfile -t "$(getimage)" .
